17fee476ca7edbdfa63b432b46044d530a0fdfa5
ROsangela <rabregum.2012@gmail.com>
2025-09-10 13:55:35 -0500

    Semana 3

---
+++ Semana03/ListaDobleEjemplo/ListaDobleEjemplo/Lista.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDobleEjemplo
+{
+    public class Lista
+    {
+
+        private Nodo Cabecera;
+        private int Contador;
+
+        public Lista()
+        {
+            Cabecera = null;
+            Contador = 0;
+        }
+
+
+        public void agregarAlUltimo(string Nombre, int Nota)
+                  {
+            Nodo Nuevo = new Nodo(Nombre, Nota);
+            if (Contador == 0) // Es el primero de la lista
+            {
+                Cabecera = Nuevo;
+                Contador++;
+                return;
+            }
+            Contador++;
+            Nodo Actual = Cabecera;
+            while(Actual.Siguiente != null)
+            {
+                Actual = Actual.Siguiente;
+            }
+            Actual.Siguiente = Nuevo;
+            Nuevo.Anterior = Actual;
+        }
+
+        public void mostrarLista()
+        {
+            Console.WriteLine("LISTA");
+            if(this.Contador == 0)
+            {
+                Console.WriteLine("Lista vacia.");
+                return;
+            }
+            Console.WriteLine("Tamaño: " + this.Contador);
+            Console.Write("NODOS: ");
+            Nodo Actual = Cabecera;
+            while (Actual != null)
+            {
+                Console.Write("(" + Actual.Nombre + "," + Actual.Nota + ") ");
+                Actual = Actual.Siguiente;
+            }
+            Console.WriteLine("");
+        }
+
+        public Nodo ObtenerElMejor()
+        {
+            if(this.Cabecera == null)
+            {
+                return null;
+            }
+            Nodo Mejor = Cabecera;
+            Nodo Actual = Mejor.Siguiente;
+            while(Actual != null)
+            {
+                if(Actual.Nota > Mejor.Nota)
+                {
+                    Mejor = Actual;
+                }
+                Actual = Actual.Siguiente;
+            }
+            return Mejor;
+        }
+
+    }
+}
---
+++ Semana03/ListaDobleEnlazada/ListaDobleEnlazada/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDobleEnlazada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList list = new DoublyLinkedList();
+            list.AddFirst(5);
+            list.AddFirst(4);
+            list.AddLast(6);
+            list.AddLast(7);
+
+            Console.WriteLine("Lista inicial:");
+            list.PrintList();  // Output: Lista inicial: 4 5 6 7
+
+            list.Remove(5);
+
+            Console.WriteLine("Lista después de eliminar 5:");
+            list.PrintList();  // Output: Lista después de eliminar 5: 4 6 7
+
+            Console.WriteLine("Número de nodos: " + list.Count);  // Output: Número de nodos: 3
+        
+            Console.ReadKey();
+        }
+    }
+}
