d5443d6212c54d49309e8dbed49a7b2b16d17c1c
Eric Gustavo Coronel Castillo <gcoronel@vallegrande.edu.pe>
2025-10-14 18:00:38 -0500

    Semana 8

---
+++ Semana08/PreviasArboles/PreviasArboles/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreviasArboles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Factorial de 5 es: " + Recursividad.Factorial(5));
+            Console.ReadKey();  
+        }
+    }
+}
---
+++ Semana08/PreviasArboles/PreviasArboles/Recursividad.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreviasArboles
+{
+    public class Recursividad
+    {
+
+        public static int Factorial(int n)
+        {
+            // Validación de entrada
+            if (n < 0)
+            {
+                throw new ArgumentException("El número debe ser no negativo.");
+            }
+            // Caso base
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+            // Caso recursivo
+            return n * Factorial(n - 1);
+        }
+
+
+
+    }
+}
